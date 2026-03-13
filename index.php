57845c6c2827faa4b02049493855ab05c22f042d
beaulauwerijssen <bjl@ziggo.nl>
2026-03-13 14:02:24 +0100

    home pagina v2

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 
 <body>
     <div class="container">
+        <a href="taakverdeling.php" class=""
         <p class="welkom">Welkom op de website</p>
         <img class="logo" src="img/placeholder.png" alt="placeholder" width="400px">
     </div>
---
+++ taakverdeling.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <title></title>
+    <?php require_once 'head.php'; ?>
+</head>
+
+</html>
