4bc3e0bcd6d079e6b0ffe12a6760b9c91d9c8e01
Elias2025 <wahbeelias1@gmail.com>
2026-03-20 12:07:40 +0100

    database veranderd

--- doc/takenlijst.sql
+++ doc/takenlijst.sql
@@ -1,36 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 20, 2026 at 11:05 AM
+-- Server version: 8.4.3
+-- PHP Version: 8.3.16
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
-DROP DATABASE IF EXISTS `takenlijst`;
-CREATE DATABASE IF NOT EXISTS `takenlijst`
-DEFAULT CHARACTER SET utf8mb4
-COLLATE utf8mb4_general_ci;
 
-USE `takenlijst`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-DROP TABLE IF EXISTS `taken`;
-CREATE TABLE IF NOT EXISTS `taken` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Database: `takenlijst`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `taken`
+--
+
+CREATE TABLE `taken` (
+  `id` int NOT NULL,
   `titel` varchar(255) NOT NULL,
   `beschrijving` text NOT NULL,
   `afdeling` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL DEFAULT 'todo',
   `deadline` date DEFAULT NULL,
-  `user` int(11) DEFAULT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `user` int DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Dumping data for table `taken`
+--
+
+INSERT INTO `taken` (`id`, `titel`, `beschrijving`, `afdeling`, `status`, `deadline`, `user`, `created_at`) VALUES
+(1, 'Test taak 1', 'Beschrijving 1', 'techniek', 'todo', NULL, NULL, '2026-03-18 14:19:41'),
+(2, 'Test taak 2', 'Beschrijving 2', 'horeca', 'done', NULL, NULL, '2026-03-18 14:19:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
   `naam` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `users` (`naam`, `username`, `password`) VALUES
-('Testgebruiker 1', 'user1', '$2y$10$XQwbcsOWgM0KvAbya2Ad2efBwTLra2CzeduJtAuY8.BW9EHx.cFKa'),
-('Testgebruiker 2', 'user2', '$2y$10$HoDxSJa/4NcFcJ.U.kj9N.cSBgcm75IwUkdgxJhLjRXY/K2cP8Fl.'),
-('Testgebruiker 3', 'user3', '$2y$10$M7vkYfdWMYqLzvCqjlOh7.nPc79zwDxtItUOh/91teGikS/XrpNuO');
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `naam`, `username`, `password`) VALUES
+(1, 'Testgebruiker 1', 'user1', '$2y$10$XQwbcsOWgM0KvAbya2Ad2efBwTLra2CzeduJtAuY8.BW9EHx.cFKa'),
+(2, 'Testgebruiker 2', 'user2', '$2y$10$HoDxSJa/4NcFcJ.U.kj9N.cSBgcm75IwUkdgxJhLjRXY/K2cP8Fl.'),
+(3, 'Testgebruiker 3', 'user3', '$2y$10$M7vkYfdWMYqLzvCqjlOh7.nPc79zwDxtItUOh/91teGikS/XrpNuO');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `taken`
+--
+ALTER TABLE `taken`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `taken`
+--
+ALTER TABLE `taken`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
