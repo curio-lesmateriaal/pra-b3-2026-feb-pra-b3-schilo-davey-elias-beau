e1d787d660fe001f7b1a0f357244fa63bbbe7ec4
beaulauwerijssen <bjl@ziggo.nl>
2026-03-27 12:40:37 +0100

    verwijder knop gemaakt op done pagina

--- done.php
+++ done.php
@@ -5,25 +5,37 @@
     <h1>Done</h1>
 
     <?php
-    $query = "SELECT titel, afdeling FROM taken WHERE status = 'done'";
+    // Haal ook de ID op, anders kun je niet linken naar edit.php of delete
+    $query = "SELECT id, titel, afdeling FROM taken WHERE status = 'done'";
     $statement = $conn->prepare($query);
     $statement->execute();
-    
+
     $result = $statement->get_result();
     $taken = $result->fetch_all(MYSQLI_ASSOC);
     ?>
+
     <table border="1" cellpadding="10" cellspacing="0">
         <thead>
             <tr>
                 <th>Titel</th>
                 <th>Afdeling</th>
+                <th>Verwijderen</th>
             </tr>
         </thead>
         <tbody>
             <?php foreach ($taken as $taak): ?>
                 <tr>
-                    <td><?php echo htmlspecialchars($taak['titel']); ?></td>
-                    <td><?php echo htmlspecialchars($taak['afdeling']); ?></td>
+                    <td><?= htmlspecialchars($taak['titel']) ?></td>
+                    <td><?= htmlspecialchars($taak['afdeling']) ?></td>
+                    <td>
+                        <form action="backend/tasksController.php?action=delete" 
+                              method="POST"
+                              onsubmit="return confirm('Weet u zeker dat u deze taak wilt verwijderen?');">
+                            
+                            <input type="hidden" name="id" value="<?= $taak['id'] ?>">
+                            <button type="submit">Verwijderen</button>
+                        </form>
+                    </td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
