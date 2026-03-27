19608791f979a458b10af5af92643734482c04cb
Elias2025 <wahbeelias1@gmail.com>
2026-03-27 12:33:23 +0100

    ik heb het done gemaakt

--- header.php
+++ header.php
@@ -1,9 +1,10 @@
 <header>
     <div class="headerContainer">
         <nav>
-            <a href="http://pra-b3-2026-feb-pra-b3-schilo-davey-elias-beau.test/index.php">Home</a>
-            <a href="http://pra-b3-2026-feb-pra-b3-schilo-davey-elias-beau.test/tasks/create.php">Nieuwe taak</a>
-            <a href="http://pra-b3-2026-feb-pra-b3-schilo-davey-elias-beau.test/login.php">Login</a>
+            <a href="/index.php">Home</a>
+            <a href="/tasks/create.php">Nieuwe taak</a>
+            <a href="/done.php">Done</a>
+            <a href="/login.php">Login</a>
         </nav>
     </div>
 </header>
